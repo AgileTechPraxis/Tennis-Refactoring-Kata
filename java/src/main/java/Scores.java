75a2fcd4f9225ec7d9bd09bad5f09b05e3375f5d
Marco Consolaro <marco@alcortech.net>
2020-11-02 09:40:32 +0000

    changed getEqualScore to Array

--- java/src/main/java/Scores.java
+++ java/src/main/java/Scores.java
@@ -5,16 +5,10 @@ class Scores {
         return SCORES[pointsPlayer1] + "-" + SCORES[pointsPlayer2];
     }
 
-    public static String getEqualScore(int pointsPlayer1) {
-        switch (pointsPlayer1) {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            default:
-                return "Deuce";
+    public static String getEqualScore(int points) {
+        if (points < 3) {
+            return SCORES[points] + "-All";
         }
+        return "Deuce";
     }
 }
