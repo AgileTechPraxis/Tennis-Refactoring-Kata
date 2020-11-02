c40ecc1225cb90a792255152c335570fbb7b05bb
Marco Consolaro <marco@alcortech.net>
2020-11-02 08:45:35 +0000

    getRegularScore

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -21,34 +21,37 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
         if (pointsPlayer1 == pointsPlayer2) {
             return getEqualScore();
         }
         if (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) {
             return getAdvantageOrWinScore();
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = pointsPlayer1;
-                else {
-                    score += "-";
-                    tempScore = pointsPlayer2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+        }
+        return getRegularScore();
+    }
+
+    private String getRegularScore() {
+        String score = "";
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = pointsPlayer1;
+            else {
+                score += "-";
+                tempScore = pointsPlayer2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
         return score;
