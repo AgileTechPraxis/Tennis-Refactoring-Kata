49a21d7f7e673fbae4bc97efb2a46cbb0df6d216
Marco Consolaro <marco@alcortech.net>
2020-11-02 08:38:10 +0000

    made switch case cleaner

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -59,22 +59,15 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getEqualScore() {
-        String score;
         switch (pointsPlayer1) {
             case 0:
-                score = "Love-All";
-                break;
+                return "Love-All";
             case 1:
-                score = "Fifteen-All";
-                break;
+                return "Fifteen-All";
             case 2:
-                score = "Thirty-All";
-                break;
+                return "Thirty-All";
             default:
-                score = "Deuce";
-                break;
-
+                return "Deuce";
         }
-        return score;
     }
 }
