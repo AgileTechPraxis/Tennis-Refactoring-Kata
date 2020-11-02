cb8bd1f5920eab04d06ed7d445ac07e33533ebdc
Marco Consolaro <marco@alcortech.net>
2020-11-02 10:18:12 +0000

    Refacted wonPoint()

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -12,25 +12,25 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
+        String score;
         if (player1Points < 4 && player2Points < 4 && !(player1Points + player2Points == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[player1Points];
-            return (player1Points == player2Points) ? s + "-All" : s + "-" + p[player2Points];
+            score = p[player1Points];
+            return (player1Points == player2Points) ? score + "-All" : score + "-" + p[player2Points];
         } else {
             if (player1Points == player2Points)
                 return "Deuce";
-            s = player1Points > player2Points ? player1Name : player2Name;
-            return ((player1Points - player2Points)*(player1Points - player2Points) == 1) ? "Advantage " + s : "Win for " + s;
+            score = player1Points > player2Points ? player1Name : player2Name;
+            return ((player1Points - player2Points)*(player1Points - player2Points) == 1) ? "Advantage " + score : "Win for " + score;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1")) {
             this.player1Points += 1;
-        else
-            this.player2Points += 1;
-
+            return;
+        }
+        this.player2Points += 1;
     }
 
 }
