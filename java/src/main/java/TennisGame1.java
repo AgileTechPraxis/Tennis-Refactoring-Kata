b27c4ebcb66253e2e3991be3a3665d1ce552dda5
Marco Consolaro <marco@alcortech.net>
2020-11-02 08:33:19 +0000

    rename member fields

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,9 +1,10 @@
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
+
+    private int pointsPlayer1 = 0;
+    private int pointsPlayer2 = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,18 +13,18 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals(player1Name)) {
-            m_score1 += 1;
+            pointsPlayer1 += 1;
         }
         if (playerName.equals(player2Name)) {
-            m_score2 += 1;
+            pointsPlayer2 += 1;
         }
     }
 
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (pointsPlayer1 == pointsPlayer2) {
+            switch (pointsPlayer1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -38,18 +39,18 @@ public class TennisGame1 implements TennisGame {
                     break;
 
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
+        } else if (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) {
+            int minusResult = pointsPlayer1 - pointsPlayer2;
             if (minusResult == 1) score = "Advantage player1";
             else if (minusResult == -1) score = "Advantage player2";
             else if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
+                if (i == 1) tempScore = pointsPlayer1;
                 else {
                     score += "-";
-                    tempScore = m_score2;
+                    tempScore = pointsPlayer2;
                 }
                 switch (tempScore) {
                     case 0:
