2582ec15ec238501630c2c6aba6e93142a6a43a0
Marco Consolaro <marco@alcortech.net>
2021-01-27 09:08:44 +0000

    r

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -23,6 +23,48 @@ public class TennisGame1 implements TennisGame {
         return inProgressScore();
     }
 
+    private boolean playersHaveSamePoints() {
+        return pointsPlayerOne == pointsPlayerTwo;
+    }
+
+    private String scoreForTie() {
+        String score;
+        switch (pointsPlayerOne) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private boolean onePlayerHasMoreThan3Points() {
+        return pointsPlayerOne >= 4 || pointsPlayerTwo >= 4;
+    }
+
+    private String scoreForAdvantageOrWin() {
+        String score;
+        int minusResult = pointsPlayerOne - pointsPlayerTwo;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
     private String inProgressScore() {
         String score = "";
         int tempScore = 0;
@@ -49,47 +91,5 @@ public class TennisGame1 implements TennisGame {
         }
 
         return score;
-    }
-
-    private String scoreForAdvantageOrWin() {
-        String score;
-        int minusResult = pointsPlayerOne - pointsPlayerTwo;
-        if (minusResult == 1) {
-            score = "Advantage player1";
-        } else if (minusResult == -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
-        }
-        return score;
-    }
-
-    private boolean onePlayerHasMoreThan3Points() {
-        return pointsPlayerOne >= 4 || pointsPlayerTwo >= 4;
-    }
-
-    private String scoreForTie() {
-        String score;
-        switch (pointsPlayerOne) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-        return score;
-    }
-
-    private boolean playersHaveSamePoints() {
-        return pointsPlayerOne == pointsPlayerTwo;
     }
 }
