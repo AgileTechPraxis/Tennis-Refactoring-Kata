38c80cfeea5ee6458c5d933b40d4b71cbb999d4f
Marco Consolaro <marco@alcortech.net>
2021-01-27 08:27:18 +0000

    r

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -36,33 +36,40 @@ public class TennisGame1 implements TennisGame {
         }
         if (pointsPlayerOne >= 4 || pointsPlayerTwo >= 4) {
             int minusResult = pointsPlayerOne - pointsPlayerTwo;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = pointsPlayerOne;
-                else {
-                    score += "-";
-                    tempScore = pointsPlayerTwo;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
+            return score;
+        }
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = pointsPlayerOne;
+            else {
+                score += "-";
+                tempScore = pointsPlayerTwo;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
+
         return score;
     }
 }
